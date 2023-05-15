355311177735ea9a5e7627666f24993e70501942
GComRui <1505956111@qq.com>
2023-05-15 09:16:17 +0800

    视图函数

--- oldWestCircle/administrator/urls.py
+++ oldWestCircle/administrator/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from administrator import views
 
 urlpatterns = [
-    path('test', views.admin_test)
+    path('test', views.admin_test),
+    path('activity', views.publish_activity),
+    path('notice', views.publish_notice),
+    path('userSelect', views.user_select),
+    path('userAdd', views.user_add),
+    path('userDelete', views.user_delete),
 ]
--- oldWestCircle/administrator/views.py
+++ oldWestCircle/administrator/views.py
@@ -6,3 +6,49 @@ from django.shortcuts import render, HttpResponse
 def admin_test(request):
     return HttpResponse("this is admin test")
 
+
+def publish_activity(request):
+    """
+    活动发布
+    @param request:
+    @return:
+    """
+    pass
+
+
+def publish_notice(request):
+    """
+    公告发布
+    @param request:
+    @return:
+    """
+    pass
+
+
+def user_select(request):
+    """
+    用户查看
+    @param request:
+    @return:
+    """
+    pass
+
+
+def user_add(request):
+    """
+    用户添加
+    @param request:
+    @return:
+    """
+    return HttpResponse("userAdd")
+
+
+def user_delete(request):
+    """
+    用户删除
+    @param request:
+    @return:
+    """
+    pass
+
+
