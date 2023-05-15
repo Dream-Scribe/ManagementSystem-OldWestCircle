194279d3b5da9e942303d21bcb765c720b350e26
GComRui <1505956111@qq.com>
2023-05-15 09:42:21 +0800

    移除了 user app

--- oldWestCircle/user/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-# Create your views here.
-
-
-def test(request):
-    # return HttpResponse("this is a test")
-    return render(request, "userTest/userTest.html")
-
